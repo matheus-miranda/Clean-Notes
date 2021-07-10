5916a05224b8e950775141ea15269cbe5bb29914
matheus-miranda <matheus.sm86@gmail.com>
2021-07-10 14:23:55 -0300

    Create RecyclerView layout and Note Item

--- app/src/main/java/br/com/mmdevelopment/cleannotes/MainActivity.kt
+++ app/src/main/java/br/com/mmdevelopment/cleannotes/MainActivity.kt
@@ -3,6 +3,8 @@ package br.com.mmdevelopment.cleannotes
 import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
+import android.transition.Explode
+import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
 import br.com.mmdevelopment.cleannotes.databinding.ActivityMainBinding
 import br.com.mmdevelopment.cleannotes.ui.AddNoteActivity
@@ -13,10 +15,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        animationTransitions()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         insertListeners()
+    }
+
+    /**
+     * Creates transition animations
+     */
+    private fun animationTransitions() {
+        with(window) {
+            requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS)
+            // set the transition to be shown when the user enters this activity
+            enterTransition = Explode()
+            enterTransition.duration = 600
+        }
     }
 
     private fun insertListeners() {
