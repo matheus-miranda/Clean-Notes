494a6898337aec8a3f5cbf98ec7997da98064cf8
matheus-miranda <matheus.sm86@gmail.com>
2021-07-09 23:52:41 -0300

    Create DatePicker dialog

--- app/src/main/java/br/com/mmdevelopment/cleannotes/ui/AddNoteActivity.kt
+++ app/src/main/java/br/com/mmdevelopment/cleannotes/ui/AddNoteActivity.kt
@@ -3,11 +3,38 @@ package br.com.mmdevelopment.cleannotes.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.mmdevelopment.cleannotes.databinding.ActivityAddNoteBinding
+import br.com.mmdevelopment.cleannotes.extensions.format
+import br.com.mmdevelopment.cleannotes.extensions.text
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.util.*
 
 class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    /**
+     * onClickListeners for Date and Time pickers
+     */
+    private fun insertListeners() {
+        binding.tilDate.editText?.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker().build()
+            datePicker.addOnPositiveButtonClickListener { date ->
+                val timeZone = TimeZone.getDefault()
+                val offset = timeZone.getOffset(Date().time) * -1
+                binding.tilDate.text = Date(date + offset).format()
+            }
+            datePicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+        }
+        binding.tilTime.editText?.setOnClickListener {
+
+        }
     }
 }
