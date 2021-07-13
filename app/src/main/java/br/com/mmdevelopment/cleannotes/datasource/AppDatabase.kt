5280949cf744838fda486b5c2f4b30b9561e4b39
matheus-miranda <matheus.sm86@gmail.com>
2021-07-13 00:06:56 -0300

    Create AppDatabase.kt

---
+++ app/src/main/java/br/com/mmdevelopment/cleannotes/datasource/AppDatabase.kt
@@ -0,0 +1,28 @@
+package br.com.mmdevelopment.cleannotes.datasource
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [NoteDao::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "notes_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
