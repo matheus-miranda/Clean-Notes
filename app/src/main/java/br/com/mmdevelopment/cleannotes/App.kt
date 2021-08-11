29fb155800f7dbc7f63339837f909a3eaf0ee8f1
matheus-miranda <matheus.sm86@gmail.com>
2021-08-11 20:26:20 -0300

    Create viewModel Koin module

--- app/src/main/java/br/com/mmdevelopment/cleannotes/App.kt
+++ app/src/main/java/br/com/mmdevelopment/cleannotes/App.kt
@@ -2,6 +2,7 @@ package br.com.mmdevelopment.cleannotes
 
 import android.app.Application
 import br.com.mmdevelopment.cleannotes.datasource.di.dataModule
+import br.com.mmdevelopment.cleannotes.ui.di.vmModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -12,7 +13,7 @@ class App : Application() {
 
         startKoin {
             androidContext(this@App)
-            modules(dataModule)
+            modules(dataModule, vmModule)
         }
     }
 }
---
+++ app/src/main/java/br/com/mmdevelopment/cleannotes/ui/di/ViewModelModule.kt
@@ -0,0 +1,12 @@
+package br.com.mmdevelopment.cleannotes.ui.di
+
+import br.com.mmdevelopment.cleannotes.ui.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val vmModule = module {
+
+    viewModel {
+        MainViewModel()
+    }
+}
