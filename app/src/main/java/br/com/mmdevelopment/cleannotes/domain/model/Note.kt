ab11bc6b3efef8d7db7df47f250c157ded219a55
matheus-miranda <matheus.sm86@gmail.com>
2021-12-17 09:37:46 -0300

    Create Note model class

---
+++ app/src/main/java/br/com/mmdevelopment/cleannotes/domain/model/Note.kt
@@ -0,0 +1,13 @@
+package br.com.mmdevelopment.cleannotes.domain.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Note(
+    var id: Int = 0,
+    val title: String,
+    val description: String,
+    val date: String,
+    val time: String
+) : Parcelable
