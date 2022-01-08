82f536aa90ec8ce3decbde9c536e90406fab26bd
matheus-miranda <matheus.sm86@gmail.com>
2022-01-08 14:14:29 -0300

    [test] Create Note model test

---
+++ app/src/test/java/br/com/mmdevelopment/cleannotes/util/TestData.kt
@@ -0,0 +1,9 @@
+package br.com.mmdevelopment.cleannotes.util
+
+import br.com.mmdevelopment.cleannotes.domain.model.Note
+
+object TestData {
+
+    val NOTE =
+        Note(title = "Title", description = "Description", date = "01/01/2022", time = "14:00")
+}
