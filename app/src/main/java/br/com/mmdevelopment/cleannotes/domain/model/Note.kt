02bf122f466736d17407d7220af67308a5e81c02
matheus-miranda <matheus.sm86@gmail.com>
2022-01-08 18:09:41 -0300

    [test] Create HomePresenter tests

--- app/src/main/java/br/com/mmdevelopment/cleannotes/domain/model/Note.kt
+++ app/src/main/java/br/com/mmdevelopment/cleannotes/domain/model/Note.kt
@@ -1,13 +1,9 @@
 package br.com.mmdevelopment.cleannotes.domain.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class Note(
     var id: Int = 0,
     val title: String,
     val description: String?,
     val date: String?,
     val time: String?
-) : Parcelable
+)
